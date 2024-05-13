99a11c0f66b78397ddcd560584e509476bdcf155
Rafael Coutinho <coutinhor@knowfully.com>
2024-05-13 19:12:07 -0300

    added transactions on UoW

--- MedPro.Infrastructure/Persistence/Repositories/IUnitOfWork.cs
+++ MedPro.Infrastructure/Persistence/Repositories/IUnitOfWork.cs
@@ -5,8 +5,8 @@ namespace MedPro.Infrastructure.Persistence.Repositories;
 public interface IUnitOfWork
 {
     ISpecialityRepository Specialities { get; }
-    
     IUserRepository Users { get;  }
-
+    Task BeginTransactionAsync();
+    Task CommitAsync();
     Task<int> CompleteAsync();
 }
--- MedPro.Infrastructure/Persistence/Repositories/UnitOfWork.cs
+++ MedPro.Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -1,11 +1,13 @@
 ﻿using MedPro.Domain.Repositories;
 using MedPro.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace MedPro.Infrastructure.Persistence.Repositories;
 
 public class UnitOfWork : IUnitOfWork
 {
     private readonly MedProDbContext _dbContext;
+    private IDbContextTransaction _transaction;
 
     public UnitOfWork(MedProDbContext dbContext, ISpecialityRepository specialities, IUserRepository users)
     {
@@ -16,6 +18,27 @@ public class UnitOfWork : IUnitOfWork
     
     public ISpecialityRepository Specialities { get; }
     public IUserRepository Users { get; }
+    
+    // To use the transaction you need to first call the BeginTransactionAsync() method, then
+    // Do the operation with objects (add/update/delete) and call CompleteAsync(), and finally
+    // Call the CommitAsync() to commit all changes
+    public async Task BeginTransactionAsync()
+    {
+        _transaction = await _dbContext.Database.BeginTransactionAsync();
+    }
+
+    public async Task CommitAsync()
+    {
+        try
+        {
+            await _transaction.CommitAsync();
+        }
+        catch (Exception)
+        {
+            await _transaction.RollbackAsync();
+            throw;
+        }
+    }
 
     public async Task<int> CompleteAsync()
     {
